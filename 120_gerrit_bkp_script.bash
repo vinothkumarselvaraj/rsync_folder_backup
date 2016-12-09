649accfab086a11f897870b81e48b76c0f281d09
Vinoth Kumar Selvaraj <vinothkumar6664@me.com>
2016-12-09 16:28:52 +0530

    Update 120_gerrit_bkp_script.bash
    if else added

--- 120_gerrit_bkp_script.bash
+++ 120_gerrit_bkp_script.bash
@@ -3,7 +3,6 @@
 # Author: Vinoth Kumar Selvaraj
 # E-Mail: vinothkumar.s@cloudenablers.com
 
-
 now=$(date +%Y_%m_%d_%H_%M_%S)
 mkdir /Gerrit_archive/Gerrit_backup_$now
 
@@ -12,12 +11,21 @@ tar -cf /Gerrit_archive/Gerrit_backup_$now.tar /Gerrit_archive/Gerrit_backup_$no
 rm -rf /Gerrit_archive/Gerrit_backup_$now
 ls -t /Gerrit_archive | sed -e '1,7d' | xargs -d '\n' rm
 
-rsync -auHxv  root@192.168.1.135:/home/ubuntu/gerrit/review_site/*  /Gerrit_rsync_bkp/
+rsync -auHxv --log-file=/var/log/rsync/gerrit_rsync_bkp.log  root@192.168.1.135:/home/ubuntu/gerrit/review_site/*  /Gerrit_rsync_bkp/
 if [ $? -eq 0 ]
 then
         subject="Gerrit_Rsync_backup_completed"
         from="cloudlab@cloudenablers.com"
         recipients="vinothkumar.s@cloudenablers.com,benyraja.j@cloudenablers.com,sushmitha.g@cloudenablers.com"
-        mail="Rsync Backup for Gerrit folders is completed at `date` Backup_Hostname:`hostname -f` IP_Detail:`hostname -I` `date`"
-        echo $mail | mail -s $subject -a "from:$from"  "$recipients"
+        truncate -s0 /var/log/rsync/gerrit_rsync_bkp_report.txt
+        echo -e "Rsync Backup for Gerrit folders is completed at `date`\n\nBackup_Hostname:`hostname -f`\nIP_Detail:`hostname -I` \n\n\nNew DATA Pulled:\n" > /var/log/rsync/gerrit_rsync_bkp_report.txt
+        tail -n2 /var/log/rsync/gerrit_rsync_bkp.log >> /var/log/rsync/gerrit_rsync_bkp_report.txt
+        mail -s $subject -a "from:$from"  "$recipients" < /var/log/rsync/gerrit_rsync_bkp_report.txt
+else
+        subject="IMPORTANT:Gerrit_Rsync_backup_FAILED"
+        from="cloudlab@cloudenablers.com"
+        recipients="vinothkumar.s@cloudenablers.com,benyraja.j@cloudenablers.com,kalaivani.v@cloudenablers.com"
+        truncate -s0 /var/log/rsync/gerrit_rsync_bkp_report.txt
+        tail -n10 /var/log/rsync/gerrit_rsync_bkp.log >> /var/log/rsync/gerrit_rsync_bkp_report.txt
+        mail -s $subject -a "from:$from"  "$recipients" < /var/log/rsync/gerrit_rsync_bkp_report.txt
 fi
